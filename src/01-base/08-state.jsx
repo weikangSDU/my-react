8ca08d3e3798ac7873bee3b869f33a7c6654b1f9
weikang <weikang@cestc.cn>
2022-02-24 13:52:18 +0800

    add: 状态学习

---
+++ src/01-base/08-state.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    isShow: true,
+    count: 0
+  }
+  myRef = React.createRef()
+  render() {
+    return (
+      <div>
+        <input type='text' ref={this.myRef} />
+        <button onClick={() => { this.setState({isShow: !this.state.isShow}) }}>{this.state.isShow ? '收藏' : '取消收藏'}</button>
+        {/* 从下行代码可以看出不实用this.setState,直接修改state的值是改变了的，react不是双向绑定响应式，所以页面才没有更新 */}
+        {/* <button onClick={() => { this.state.isShow = !this.state.isShow; console.log(this.state, 'state') }}>测试state的值</button> */}
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM  from 'react-dom'
 // import App from './01-base/01-class组件'
 // import App from './01-base/05-事件绑定-1'
-import App from './01-base/07-ref'
+// import App from './01-base/07-ref'
+import App from './01-base/08-state'
 
 ReactDOM.render(<App />, document.getElementById('root'))
